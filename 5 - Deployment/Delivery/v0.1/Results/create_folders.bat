8682fad817731088d5a075bd2863f3306de9dbef
sseijas <sseijas@baufest.com>
2016-07-20 10:26:41 -0300

    para recrear las carpetas

---
+++ 5 - Deployment/Delivery/v0.1/Results/create_folders.bat
@@ -0,0 +1,19 @@
+ECHO OFF
+
+RD /S /Q SDK-Java
+RD /S /Q SDK-Net
+RD /S /Q SDK-NodeJS
+RD /S /Q SDK-PHP
+RD /S /Q SDK-Python
+RD /S /Q SDK-Ruby
+
+MKDIR SDK-Java\v1.8\Sample
+MKDIR SDK-Java\v1.8\Sample
+MKDIR SDK-Net\v4.0\Sample
+MKDIR SDK-NodeJS\v4.4.5\Sample
+MKDIR SDK-PHP\v5.3\Sample
+MKDIR SDK-Python\v2.7\Sample
+MKDIR SDK-Ruby\v1.9.3\Sample
+
+
+PAUSE
