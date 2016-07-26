e1fe645af727e69c5aaadca04d64c0a27f1edeed
sseijas <sseijas@baufest.com>
2016-07-26 09:52:48 -0300

    php with autoload release version

--- 5 - Deployment/Delivery/v0.1/Adapter/PHP/v5.3/TodoPago/lib/Exception/Data/EmptyFieldPasswordException.php
+++ 5 - Deployment/Delivery/v0.1/Adapter/PHP/v5.3/TodoPago/lib/Exception/Data/EmptyFieldPasswordException.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace TodoPago\Exception\Data;
+namespace TodoPago\Exception\Data
 
 class EmptyFieldUserException extends EmptyFieldException {
 	
--- 5 - Deployment/Delivery/v0.1/Adapter/PHP/v5.3/bin/automated.bat
+++ 5 - Deployment/Delivery/v0.1/Adapter/PHP/v5.3/bin/automated.bat
@@ -2,5 +2,5 @@ ECHO OFF
 
 DEL ..\..\..\Results\SDK-PHP\v5.3\Sample\*.out
 
-php Adapter.php /c:PaymentFlow /i:..\..\..\..\Scenarios\Sample\Sample_01_PaymentFlow.ini /o:..\..\..\..\Results\SDK-PHP\v5.3\Sample\1_PaymentFlow.out /x:..\..\..\..\Scenarios\Sample\WS_Execute_Request.xml
+php -f Adapter.php /c:PaymentFlow /i:..\..\..\..\Scenarios\Sample\Sample_01_PaymentFlow.ini /o:..\..\..\..\Results\SDK-PHP\v5.3\Sample\1_PaymentFlow.out /x:..\..\..\..\Scenarios\Sample\WS_Execute_Request.xml
 PAUSE
